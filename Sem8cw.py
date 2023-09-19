35bafe241518c3e032bc96375f3e329f8876070b
AlexKhabibov <alex.lv.u@gmail.com>
2023-09-19 15:59:58 +0300

    Sem 8 Home work

---
+++ Sem8cw.py
@@ -0,0 +1,9 @@
+# from pathlib import Path
+
+# file_path = Path('info', 'data.txt')
+# file_path = r'info/new.txt'
+# print (file_path)
+
+# # with open('data.txt', 'r', encoding='utf-8') as text_file:
+# #     for line in text_file:
+# #         print (line.strip())
